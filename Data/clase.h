ca46cb11c50846299723951606ddc555d07567b0
alex <alexandru.stefanescu04@e-uvt.ro>
2023-05-20 14:29:55 +0300

    idk

--- Data/clase.h
+++ Data/clase.h
@@ -2,9 +2,28 @@
 #include <string>
 #include <vector>
 
-class Joc{
+class Game{
     private:
         int key;
         std::string title, category;
         double size, price;
+};
+
+class Bundle{
+    private:
+        int key, discount;
+        std::string title;
+        std::vector<int> games;
+        double price;
+};
+
+class Catalog{
+    private:
+        std::vector<int> games, bundles, top5;
+};
+
+class Console{
+    private:
+        double storage, storageLeft;
+        std::vector<Game> games, gamesInstalled;
 };
